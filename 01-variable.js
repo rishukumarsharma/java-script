83b46b6082e661456d4960b99d3d96fa613b8d71
Rishu kumar sharma <rishu3826@gmail.com>
2024-01-13 00:07:09 +0530

    commite

---
+++ 01-variable.js
@@ -0,0 +1,6 @@
+const accountId=23
+let accountEmail="rishu3826@gmail.com"
+var accountPassword="3344"
+const accountCity="jaipur"
+
+console.log(accountId,accountEmail,accountPassword,accountCity);
